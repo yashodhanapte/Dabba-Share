ded40e1a2274029b2e0964208ff825e4b50ed995
Yashodhan Apte <yash@Yashodhans-MacBook-Pro.local>
2018-06-22 19:33:31 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import {MatTableDataSource} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  friends = new FormControl();
+
+  // constant rates
+  costOfLunch = 85;
+  costOfExtraRoti = 30;
+  showOrderDetails: boolean = false;
+  showReports:boolean = false;
+  displayedColumns = ['name','pendingBalance','dateStart','dateEnd'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  // available options in select boxes
+  lunches = [1, 2, 3, 4, 5, 6];
+  extraRotiRolls = [1, 2, 3, 4, 5, 6];
+  members = ['Yashodhan', 'Sandesh', 'Sumit', 'Kundan', 'Rushikesh', 'Keyur', 'Shubham', 'Manish'];
+
+  // user inputs after selection
+  numberOfLunchesSelected: number;
+  numberOfRotiRollsSelected: number;
+  todaysMembers = [];
+
+  // variables for calculations
+  todaysTotal: number;
+  todaysTotalPerHead: number;
+
+  // functions
+
+  placeOrder() {
+
+    // calculating today's total cost
+    this.todaysTotal = (this.costOfLunch * this.numberOfLunchesSelected) + (this.costOfExtraRoti * this.numberOfRotiRollsSelected);
+
+    // calculating today's cost per head
+    this.todaysTotalPerHead = (this.todaysTotal / this.todaysMembers.length);
+
+    // showing order details for today
+    this.showOrderDetails = true;
+  }
+
+  viewReports(){
+    this.showReports = true;
+  }
+
 }
+
+export interface PeriodicElement {
+  name: string;
+  pendingBalance:number;
+  dateStart:string;
+  dateEnd:string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Yashodhan',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Sandesh',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Sumit',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Kundan',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Rushikesh',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Keyur',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Shubham',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'},
+  {name: 'Manish',pendingBalance:0,dateStart:'01/06/2018',dateEnd:'30/06/2018'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule,MatButtonModule, MatCheckboxModule,MatSelectModule,MatTableModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule, MatCheckboxModule, MatSelectModule,MatInputModule,MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
