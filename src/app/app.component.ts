43007e63c5d2174ea5519f7a11a56355002eade5
Yashodhan Apte <yash@Yashodhans-MacBook-Pro.local>
2018-06-25 21:14:40 +0530

    last commit before implementing firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import {MatTableDataSource} from '@angular/material';
+import { MatTableDataSource } from '@angular/material';
+import { AngularFirestore } from 'angularfire2/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
